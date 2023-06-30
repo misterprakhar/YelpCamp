07e841d0067400e03768b7c76c77fce183034286
Prakhar Srivastav <82384669+misterprakhar@users.noreply.github.com>
2023-06-30 20:20:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ const reviewRoutes = require("./routes/reviews");
 
 mongoose.set("strictQuery", true);
 
-const dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
-// const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL||"mongodb://127.0.0.1:27017/yelp-camp";
 mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	// useCreateIndex: true,
